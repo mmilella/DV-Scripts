9bd1547d23cde915df94ff7e3fc0d722398d4af0
Matt Milella <mmilella@gmail.com>
2018-12-16 14:41:28 -0600

    Update uninstall-dvdesktop.sh

---
+++ uninstall-dvdesktop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Uninstall Data Visualization for Desktop on Mac
+echo "Uninstalling Data Visualization for Desktop on Mac..."
+
+sudo rm -rf "/Applications/Oracle Data Visualization for Desktop.app"
+sudo rm -rf "/Applications/DVDesktop.app"
+sudo rm -rf "/Applications/Oracle Data Visualization Desktop Configure Python.app"
+
+# Uninstall All Data and Projects
+echo "Uninstalling All Data and Projects..."
+
+rm -rf ~/Library/Application\ Support/DataVisualization
+rm -rf ~/Library/Application\ Support/DVDesktop
+
+echo "Finished Uninstallation process."
+
